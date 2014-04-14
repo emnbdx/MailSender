07621fea96266633d794e878fbcfc51fca1fca59
Eddy MONTUS <eddy.montus@gmail.com>
2014-04-14 22:32:12 +0000

    use 1&1 mail, some edit to be more flexible.

--- EMToolBox/Mail/EMMail.cs
+++ EMToolBox/Mail/EMMail.cs
@@ -14,34 +14,33 @@ namespace EMToolBox.Mail
     {
         private ILog log = LogManager.GetLogger(typeof(EMMail));
         
-        private static string m_serveur = "smtp.gmail.com";
         private static int m_port = 587;
-        private static string[] m_user = new string[6] { "emappmailsender@gmail.com",
-            "emappmailsender1@gmail.com",
-            "emappmailsender2@gmail.com",
-            "emappmailsender3@gmail.com",
-            "emappmailsender@gmail.com",
-            "emappmailsender@gmail.com" };
-        private static string m_password = "3m4ppm4ailS3nder";
-
+        private static string[,] m_login = new string[7, 3] { 
+            { "contact@lbcalerter.com", "vicpt240", "auth.smtp.1and1.fr"},
+            { "emappmailsender@gmail.com", "3m4ppm4ailS3nder", "smtp.gmail.com"},
+            {"emappmailsender1@gmail.com", "3m4ppm4ailS3nder", "smtp.gmail.com"},
+            {"emappmailsender2@gmail.com", "3m4ppm4ailS3nder", "smtp.gmail.com"},
+            {"emappmailsender3@gmail.com", "3m4ppm4ailS3nder", "smtp.gmail.com"},
+            {"emappmailsender4@gmail.com", "3m4ppm4ailS3nder", "smtp.gmail.com"},
+            {"emappmailsender5@gmail.com", "3m4ppm4ailS3nder", "smtp.gmail.com"} };
         private void Send(String subject, String body, String to, int attemp)
         {
             try
             {
                 MailMessage message = new MailMessage();
 
-                message.From = new MailAddress(m_user[attemp]);
+                message.From = new MailAddress(m_login[attemp, 0]);
                 message.To.Add(to);
                 message.Subject = subject;
                 message.IsBodyHtml = true;
                 message.Body = body;
 
-                using (SmtpClient smtp = new SmtpClient(m_serveur, m_port))
+                using (SmtpClient smtp = new SmtpClient(m_login[attemp, 2], m_port))
                 {
                     if (m_port != 25)
                         smtp.EnableSsl = true;
-                    if (!String.IsNullOrEmpty(m_user[attemp]) && !String.IsNullOrEmpty(m_password))
-                        smtp.Credentials = new System.Net.NetworkCredential(m_user[attemp], m_password);
+                    if (!String.IsNullOrEmpty(m_login[attemp, 0]) && !String.IsNullOrEmpty(m_login[attemp, 1]))
+                        smtp.Credentials = new System.Net.NetworkCredential(m_login[attemp, 0], m_login[attemp, 1]);
                     smtp.Send(message);
                 }
             }
@@ -54,7 +53,7 @@ namespace EMToolBox.Mail
         public void SendSmtpMail(String subject, String body, String to)
         {
             int attemps = 0;
-            while (attemps < m_user.Count())
+            while (attemps < m_login.GetLength(0))
             {
                 try
                 {

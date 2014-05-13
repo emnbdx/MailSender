ab31f6ac204681b3fa6ea7d8596143b2f903be1d
Eddy MONTUS <eddy.montus@gmail.com>
2014-05-13 00:50:40 +0000

    add service for mail management

---
+++ MailSender/MailSenderService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EMToolBox.Mail;
+using EMToolBox.Services;
+using log4net;
+using System.Configuration;
+
+namespace MailSender
+{
+    class MailSenderService : SimpleService
+    {
+        private static ILog log = LogManager.GetLogger(typeof(MailSenderService));
+        private EMMail _mailer;
+        
+        public override void Initialize()
+        {
+            base.Initialize();
+            this.Interval = Convert.ToInt32(ConfigurationManager.AppSettings["Intervalle"]) * 1000;
+            _mailer = new EMMail();
+        }
+
+        public override void Process()
+        {
+            base.Process();
+            _mailer.Send();
+        }
+    }
+}
---
+++ MailSender/Program.cs
@@ -0,0 +1,22 @@
+﻿using EMToolBox.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MailSender
+{
+    static class Program
+    {
+        /// <summary>
+        /// Point d'entrée principal de l'application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            ServiceManager.Run(typeof(MailSenderService));
+        }
+    }
+}

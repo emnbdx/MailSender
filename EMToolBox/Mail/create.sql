0c8f435acad24cd73879faec8c4d44def3fe3a15
Eddy <eddy.montus@gmail.com>
2014-09-08 18:38:03 +0200

    LBCAlerter remove commercial information getting, update mail template to delete commercial information.

--- EMToolBox/Mail/create.sql
+++ EMToolBox/Mail/create.sql
@@ -184,9 +184,7 @@ UPDATE PATTERN SET CONTENT = '<!DOCTYPE html>
 			{Title}
 		</div>
         <div style="border: solid #ccc; border-width: 0 1px 1px 1px; border-bottom-left-radius: 4px; border-bottom-right-radius: 4px; padding: 20px; font-size: 16px;">
-			Mise en ligne le {Date}, par <a href="{ContactUrl}">{Name}</a> [Phone](<img src="{Phone}" alt="telephone"/>)[/Phone]<br />
-			[AllowCommercial]{Name} accepte les démarchages commerciaux<br />[/AllowCommercial]
-			[NOT_AllowCommercial]{Name} refuse les démarchages commerciaux<br />[/NOT_AllowCommercial]<br /><br />
+			Mise en ligne le {Date}, par <a href="{ContactUrl}">{Name}</a> [Phone](<img src="{Phone}" alt="telephone"/>)[/Phone]<br /><br />
             <a href="{AdUrl}" target="_blank">
                 #PictureUrl#<img src="{PictureUrl}" alt="{AdUrl}" style="width:160px; height: 120px;" />#/PictureUrl#
             </a><br />
@@ -234,9 +232,7 @@ UPDATE PATTERN SET CONTENT = '<!DOCTYPE html>
 WHERE NAME = 'LBC_RECAP_FULL'
 
 UPDATE PATTERN SET CONTENT = '<span style="font-weight: bold; display:block; padding: 20px;">{Title}</span><br />
-Mise en ligne le {Date}, par <a href="{ContactUrl}">{Name}</a> [Phone](<img src="{Phone}" alt="telephone"/>)[/Phone]<br />
-[AllowCommercial]{Name} accepte les démarchages commerciaux<br />[/AllowCommercial]
-[NOT_AllowCommercial]{Name} refuse les démarchages commerciaux<br />[/NOT_AllowCommercial]<br /><br />
+Mise en ligne le {Date}, par <a href="{ContactUrl}">{Name}</a> [Phone](<img src="{Phone}" alt="telephone"/>)[/Phone]<br /><br />
 <a href="{Url}" target="_blank">
     #PictureUrl#<img src="{PictureUrl}" alt="{Url}" style="width:160px; height: 120px;" />#/PictureUrl#
 </a><br />
